f97088672ecb8ab99befc7610a31d90c001c1c7b
brightyein <wmfrjqsp@gmail.com>
2023-08-07 17:31:39 +0900

    initial

---
+++ src/main/java/com/example/github_test2/GithubTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.github_test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubTest2Application.class, args);
+	}
+
+}
